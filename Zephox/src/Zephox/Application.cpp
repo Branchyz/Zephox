6293b75918813994decace6c4d22f5db111847ee
Branchyz <matthijsirik715@gmail.com>
2022-07-04 11:50:35 +0200

    Fixed a little layer issue

--- Zephox/src/Zephox/Application.cpp
+++ Zephox/src/Zephox/Application.cpp
@@ -19,10 +19,12 @@ namespace Zephox {
 
 	void Application::PushLayer(Layer* layer) {
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay) {
 		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e) {
